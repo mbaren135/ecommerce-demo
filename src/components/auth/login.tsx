79f63efbd83b878577fdbc0bb3eeb2d3d6b2d5f4
mbaren135 <102633983+mbaren135@users.noreply.github.com>
2025-08-19 14:41:21 -0500

    swap color scheme on login

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -16,14 +16,14 @@ export default function LoginForm() {
   const disabled = username === "" || password === "";
 
   return (
-    <div className="flex justify-center items-center w-full h-screen bg-brand-accent">
+    <div className="flex justify-center items-center w-full h-screen bg-brand-primary">
       <div className="grid grid-cols-[2fr_1fr] w-full">
-        <div className="font-sans flex flex-col gap-4 items-center text-brand-primary justify-center w-7/8 p-6 border-brand-light-400">
+        <div className="font-sans flex flex-col gap-4 items-center text-brand-accent justify-center w-7/8 p-6 border-brand-light-400">
           <div className="flex flex-col w-3/4 m-2 gap-6">
             <div className="flex flex-col mb-5 gap-4">
               <div className="flex gap-8 items-center">
                 <ShopDropIcon size="60px" />
-                <h1 className="text-5xl text-brand-primary">
+                <h1 className="text-5xl text-brand-accent">
                   Welcome to ShopDrop!
                 </h1>
               </div>
@@ -33,7 +33,7 @@ export default function LoginForm() {
                 system. <br />
                 All users, products, and carts were found at{" "}
                 <a
-                  className="text-brand-primary-600 underline"
+                  className="text-brand-accent-400 underline"
                   target="_blank"
                   href="https://fakestoreapi.com"
                 >
@@ -43,13 +43,13 @@ export default function LoginForm() {
             </div>
             <p>Username</p>
             <input
-              className="border-1 p-1 w-full border-brand-primary rounded-sm text-brand-primary focus:outline-none focus:border-2 focus:bg-brand-accent-300"
+              className="border-1 p-1 w-full border-brand-accent rounded-sm text-brand-accent focus:outline-none focus:border-2 focus:bg-brand-primary-600 focus:text-brand-accent"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
             <p>Password</p>
             <input
-              className="border-1 p-1 w-full border-brand-primary rounded-sm text-brand-primary focus:outline-none focus:border-2 focus:bg-brand-accent-300"
+              className="border-1 p-1 w-full border-brand-accent rounded-sm text-brand-accent focus:outline-none focus:border-2 focus:bg-brand-primary-600 focus:text-brand-accent"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               onKeyDown={(e) => {
@@ -61,10 +61,10 @@ export default function LoginForm() {
             />
           </div>
           <BrandButton
-            className={`w-3/4 ${
-              disabled &&
-              "bg-brand-primary-900 text-brand-light-400 hover:text-brand-light-400 hover:cursor-not-allowed hover:scale-100 hover:shadow-none"
-            }`}
+            className={`w-3/4 text-brand-primary ${
+              disabled ?
+              "bg-brand-accent-900 text-brand-light-900 hover:text-brand-light-900 hover:cursor-not-allowed hover:scale-100 hover:shadow-none"
+            : "bg-brand-accent hover:text-brand-primary hover:font-semibold"}`}
             onClick={handleLogin}
             disabled={loading || disabled}
           >
--- src/components/user/user-skeleton.tsx
+++ src/components/user/user-skeleton.tsx
@@ -2,17 +2,17 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function UserSkeleton() {
   return (
-    <Card className=" group border-brand-light-400 rounded-lg py-3 bg-brand-primary-100">
+    <Card className=" group border-brand-light-400 rounded-lg py-3 bg-brand-accent-100">
       <CardContent className="w-full max-h-fit px-3">
         <div className="space-y-2 w-full">
-          <div className="h-5 bg-brand-primary-200 rounded animate-pulse" />
+          <div className="h-5 bg-brand-accent-200 rounded animate-pulse" />
           <div className="flex gap-4 items-center">
-            <div className="h-4 w-1/3 bg-brand-primary-200 rounded animate-pulse" />
-            <div className="h-4 w-2/3 bg-brand-primary-200 rounded animate-pulse" />
+            <div className="h-4 w-1/3 bg-brand-accent-200 rounded animate-pulse" />
+            <div className="h-4 w-2/3 bg-brand-accent-200 rounded animate-pulse" />
           </div>
           <div className="flex gap-4 items-center">
-            <div className="h-4 w-1/3 bg-brand-primary-200 rounded animate-pulse" />
-            <div className="h-4 w-2/3 bg-brand-primary-200 rounded animate-pulse" />
+            <div className="h-4 w-1/3 bg-brand-accent-200 rounded animate-pulse" />
+            <div className="h-4 w-2/3 bg-brand-accent-200 rounded animate-pulse" />
           </div>
         </div>
       </CardContent>
