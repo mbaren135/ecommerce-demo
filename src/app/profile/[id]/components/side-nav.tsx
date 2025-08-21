011bf3669f5f1fc37df80cd28034ede857b31b49
mbaren135 <102633983+mbaren135@users.noreply.github.com>
2025-08-21 01:49:53 -0500

    animate panel highlight on side bar

--- src/app/profile/[id]/components/side-nav.tsx
+++ src/app/profile/[id]/components/side-nav.tsx
@@ -1,64 +1,131 @@
-import { ReactNode } from "react";
+import { motion } from "framer-motion";
+import {
+  Dispatch,
+  forwardRef,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+type IndicatorType = { top: number; height: number; width: number };
 
 export default function SideNav({
   content,
   setContent,
 }: {
-  content: string;
-  setContent: (newContent: "details" | "cart" | "delete") => void;
+  content: "details" | "cart" | "delete";
+  setContent: Dispatch<SetStateAction<"details" | "cart" | "delete">>;
 }) {
+  const [indicator, setIndicator] = useState<IndicatorType | null>({
+    top: 0,
+    height: 0,
+    width: 0,
+  });
+
+  const firstItemRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (firstItemRef.current) {
+      const { width, height } = firstItemRef.current.getBoundingClientRect();
+      setIndicator({
+        top: firstItemRef.current.offsetTop,
+        width,
+        height,
+      });
+    }
+  }, []);
+
   return (
     <aside className="flex justify-center w-1/4 bg-brand-accent text-brand-primary">
       <div className="container flex flex-col gap-4 p-6 font-sans">
-        <ListItem addHr>
+        <div className="flex flex-col gap-4">
           <p className="text-3xl font-semibold">Your Account</p>
-        </ListItem>
-        <ListItem addHr>
-          <p
-            className={`p-2 rounded-md text-lg cursor-pointer hover:text-xl hover:underline ${
-              content === "details" && "bg-brand-accent-400"
-            }`}
-            onClick={() => setContent("details")}
+          <hr />
+        </div>
+        <ul className="relative flex flex-col gap-4">
+          <ListItem
+            ref={firstItemRef}
+            addHr
+            setContent={setContent}
+            contentValue="details"
+            setIndicator={setIndicator}
           >
             Account Details
-          </p>
-        </ListItem>
-        <ListItem addHr>
-          <p
-            className={`p-2 rounded-md text-lg cursor-pointer hover:text-xl hover:underline ${
-              content === "cart" && "bg-brand-accent-400"
-            }`}
-            onClick={() => setContent("cart")}
+          </ListItem>
+          <ListItem
+            addHr
+            setContent={setContent}
+            contentValue="cart"
+            setIndicator={setIndicator}
           >
             View Cart Drops
-          </p>
-        </ListItem>
-        <ListItem>
-          <p
-            className={`p-2 rounded-md text-lg cursor-pointer hover:text-xl hover:underline ${
-              content === "delete" && "bg-brand-accent-400"
-            }`}
-            onClick={() => setContent("delete")}
+          </ListItem>
+          <ListItem
+            setContent={setContent}
+            contentValue="delete"
+            setIndicator={setIndicator}
           >
             Delete Account
-          </p>
-        </ListItem>
+          </ListItem>
+          <Indicator indicator={indicator} />
+        </ul>
       </div>
     </aside>
   );
 }
 
-function ListItem({
-  children,
-  addHr = false,
-}: {
-  children: ReactNode;
-  addHr?: boolean;
-}) {
+const ListItem = forwardRef<
+  HTMLParagraphElement,
+  {
+    children: ReactNode;
+    contentValue: "details" | "cart" | "delete";
+    setContent: Dispatch<SetStateAction<"details" | "cart" | "delete">>;
+    setIndicator: Dispatch<SetStateAction<IndicatorType | null>>;
+    addHr?: boolean;
+  }
+>(function ListItem(
+  { children, contentValue, setContent, setIndicator, addHr = false },
+  forwardedRef
+) {
+  const internalRef = useRef<HTMLParagraphElement>(null);
+  const ref = forwardedRef || internalRef;
+
   return (
-    <div className="flex flex-col gap-4">
-      {children}
-      {addHr && <hr />}
-    </div>
+    <li
+      className="flex flex-col "
+      onClick={() => {
+        setContent(contentValue);
+
+        const currentRef = typeof ref === "function" ? null : ref?.current;
+        if (currentRef) {
+          const { width, height } = currentRef.getBoundingClientRect();
+          setIndicator((prev) => ({
+            ...prev,
+            top: currentRef.offsetTop,
+            width,
+            height,
+          }));
+        }
+      }}
+    >
+      <p
+        className="relative p-2 text-lg cursor-pointer hover:text-xl hover:underline z-10"
+        ref={ref}
+      >
+        {children}
+      </p>
+      {addHr && <hr className="mt-4" />}
+    </li>
+  );
+});
+
+function Indicator({ indicator }: { indicator: IndicatorType | null }) {
+  return (
+    <motion.li
+      animate={{ ...indicator }}
+      className="absolute rounded-md bg-brand-accent-400 z-0"
+    />
   );
 }
